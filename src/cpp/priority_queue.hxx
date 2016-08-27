97e9ba183eac9e6d526a40e2f91ce93cd981468f
Philip Schill <philip.schill@gmx.de>
2016-08-27 20:35:14 +0200

    Added doc comments to priority queue.

--- src/cpp/priority_queue.hxx
+++ src/cpp/priority_queue.hxx
@@ -6,6 +6,9 @@
 namespace dmath
 {
 
+    /**
+     * A priority queue that always has the element with the lowest score at the top.
+     */
     template <typename T, typename SCORER>
     class PriorityQueue
     {
@@ -14,26 +17,52 @@ namespace dmath
         typedef SCORER Scorer;
         typedef typename std::result_of<Scorer(T const &)>::type ScoreType;
 
+        /**
+         * Initialize the priority queue with the given scorer.
+         * The Scorer class must implement operator()(T const & x) which returns the score of item x.
+         */
         PriorityQueue(
                 Scorer const & scorer
         );
 
+        /**
+         * Insert the item into the queue.
+         */
         void push(
                 T const & item
         );
 
+        /**
+         * Remove the top element from the queue.
+         */
         void pop();
 
+        /**
+         * Return the top element.
+         */
         T const & top() const;
 
+        /**
+         * Resort all items using the stored scorer.
+         * This method should be used if the score of item x changes after x has been inserted into the queue.
+         */
         void reweight();
 
+        /**
+         * Return true if the queue contains the given item.
+         */
         bool contains(
                 T const & item
         ) const;
 
+        /**
+         * Return the size of the queue.
+         */
         size_t size() const;
 
+        /**
+         * Return whether the queue is empty.
+         */
         bool empty() const;
 
     private:
@@ -44,6 +73,11 @@ namespace dmath
 
     };
 
+    /**
+     * Create a priority queue that uses the given scorer.
+     * This method can be used to create a priority queue without explicitly naming the SCORER type:
+     * auto queue = make_priority_queue<size_t>(scorer);
+     */
     template <typename T, typename SCORER>
     PriorityQueue<T, SCORER> make_priority_queue(
             SCORER && scorer
